3a758333fe8a084ead864a868393882cf604e806
Tómas Hrafn <tomash14@ru.is>
2019-06-24 09:34:38 +0000

    update version and fixed queries syntax

--- upgrade_network.sh
+++ upgrade_network.sh
@@ -1,4 +1,4 @@
 composer archive create -t dir -n .
-composer network install -a certificate-network@0.1.28.bna -c peeradmin@hlfv1
-composer network upgrade -c peeradmin@hlfv1 -n certificate-network -V 0.1.28
+composer network install -a certificate-network@0.1.29.bna -c peeradmin@hlfv1
+composer network upgrade -c peeradmin@hlfv1 -n certificate-network -V 0.1.29
 
